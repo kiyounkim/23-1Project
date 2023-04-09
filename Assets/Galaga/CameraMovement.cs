44323eb4a53a2996b9536982ac7464b296cc7aa7
kiyounkim <21900072@handong.ac.kr>
2023-04-09 20:29:00 +0900

    camera reset

--- Assets/Galaga/CameraMovement.cs
+++ Assets/Galaga/CameraMovement.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class CameraMovement : MonoBehaviour
 {
     public Transform target;
-    public Transform look;
     public float smoothSpeed = 0.125f;
+    public Vector3 offset;
 
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position;
+        Vector3 desiredPosition = target.position + offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-        transform.LookAt(look);
+        transform.LookAt(target);
     }
 }
