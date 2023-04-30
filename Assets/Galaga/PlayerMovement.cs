aa231901a677f47006bdf98e720d80f11c93fa49
kiyounkim <21900072@handong.ac.kr>
2023-04-30 20:17:59 +0900

    prefabs made

--- Assets/Galaga/PlayerMovement.cs
+++ Assets/Galaga/PlayerMovement.cs
@@ -28,17 +28,24 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontalInput, Mathf.Abs(Input.GetAxis("Fire3")) > 0 ? -Input.GetAxis("Fire3") : Input.GetAxis("Jump"), verticalInput);
-        rigidbody.AddForce(movement);
+        rigidbody.AddForce(movement*2);
         
-        //movement tilt
+        //aim
+        /*
+        float horizontalRotation = Input.GetAxis("Mouse X");
+        float verticalRotation = Input.GetAxis("Mouse Y");
+        transform.Rotate(Vector3.up, horizontalRotation * Time.deltaTime * 100f, Space.World);
+        transform.Rotate(Vector3.left, verticalRotation * Time.deltaTime * 100f, Space.Self);
+        */
+        //movement
+        /*
         float tiltX = verticalInput * tiltAngle * 2;
         float tiltZ = -horizontalInput * tiltAngle * 2;
         Quaternion targetTilt = Quaternion.Euler(tiltX, 0, tiltZ);
         transform.rotation = Quaternion.Slerp(transform.rotation, targetTilt, tiltSpeed * Time.deltaTime);
-        
-        
-        if(Input.GetKeyDown(KeyCode.LeftControl)) rigidbody.velocity = Vector3.zero;
+        */
 
+        if(Input.GetKeyDown(KeyCode.LeftControl)) rigidbody.velocity = Vector3.zero;
         // Add this line of code to show the ray in the game
         lineRenderer.SetPosition(0, transform.position);
         lineRenderer.SetPosition(1, transform.position + transform.forward * 10);
